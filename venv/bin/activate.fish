0d7ecce0eadf4d6e8e8e376cf8c24c601d76c631
ddfliu <ddfliu@users.noreply.github.com>
2026-05-17 01:40:30 +0000

    feat: 项目说明与测试
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ddfightliu/myGitProjs/geoflow/venv
+set -gx VIRTUAL_ENV /workspace/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
